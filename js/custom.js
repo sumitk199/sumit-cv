bbc9477375ee6246d9d3898f8d2da6e487f79593
sumitk199 <kumawatsumit199@gmail.com>
2023-10-23 11:41:32 +0530

    new section added

--- js/custom.js
+++ js/custom.js
@@ -57,3 +57,79 @@ $(function () {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
+
+//Tap to scroll top
+const button = document.querySelector(".T-T-S-btn");
+
+const displayButton = () => {
+  window.addEventListener("scroll", () => {
+    console.log(window.scrollY);
+
+    if (window.scrollY > 100) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  });
+};
+
+const scrollToTop = () => {
+  button.addEventListener("click", () => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    console.log(event);
+  });
+};
+
+displayButton();
+scrollToTop();
+
+// slider front end developer
+
+// var words = ["Front End Developer", "Develop unique websites"],
+//   part,
+//   i = 0,
+//   offset = 0,
+//   len = words.length,
+//   forwards = true,
+//   skip_count = 0,
+//   skip_delay = 15,
+//   speed = 70;
+// var wordflick = function () {
+//   setInterval(function () {
+//     if (forwards) {
+//       if (offset >= words[i].length) {
+//         ++skip_count;
+//         if (skip_count == skip_delay) {
+//           forwards = false;
+//           skip_count = 0;
+//         }
+//       }
+//     } else {
+//       if (offset == 0) {
+//         forwards = true;
+//         i++;
+//         offset = 0;
+//         if (i >= len) {
+//           i = 0;
+//         }
+//       }
+//     }
+//     part = words[i].substr(0, offset);
+//     if (skip_count == 0) {
+//       if (forwards) {
+//         offset++;
+//       } else {
+//         offset--;
+//       }
+//     }
+//     $(".word").text(part);
+//   }, speed);
+// };
+
+// $(document).ready(function () {
+//   wordflick();
+// });
