6ed1d73c75469e32d54cfc21a5bd22625086e0cb
sumitk199 <kumawatsumit199@gmail.com>
2024-01-01 20:49:09 +0530

    new html

--- js/custom.js
+++ js/custom.js
@@ -10,18 +10,18 @@ $(document).on("click", 'a[href^="#"]', function (event) {
 });
 
 // slides
-$(document).ready(function () {
-  $(".projects ul").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-    // prevArrow: '<button class="slide-arrow prev-arrow"></button>',
-    // nextArrow: '<button class="slide-arrow next-arrow"></button>',
-  });
-});
+// $(document).ready(function () {
+//   $(".projects ul").slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//     arrows: false,
+//     // prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+//     // nextArrow: '<button class="slide-arrow next-arrow"></button>',
+//   });
+// });
 
 // loader
 $(document).ready(function () {
@@ -40,38 +40,46 @@ $(document).ready(function () {
 });
 
 //scroll header
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".h-bg");
-    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-  });
-});
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".h-bg");
+//     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//   });
+// });
 
 //Tap to scroll top
-const button = document.querySelector(".T-T-S-btn");
+// const button = document.querySelector(".T-T-S-btn");
 
-const displayButton = () => {
-  window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
+// const displayButton = () => {
+//   window.addEventListener("scroll", () => {
+//     console.log(window.scrollY);
 
-    if (window.scrollY > 100) {
-      button.style.display = "block";
-    } else {
-      button.style.display = "none";
-    }
-  });
-};
+//     if (window.scrollY > 100) {
+//       button.style.display = "block";
+//     } else {
+//       button.style.display = "none";
+//     }
+//   });
+// };
 
-const scrollToTop = () => {
-  button.addEventListener("click", () => {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
-    console.log(event);
-  });
-};
+// const scrollToTop = () => {
+//   button.addEventListener("click", () => {
+//     window.scroll({
+//       top: 0,
+//       left: 0,
+//       behavior: "smooth",
+//     });
+//     console.log(event);
+//   });
+// };
 
-displayButton();
-scrollToTop();
+// displayButton();
+// scrollToTop();
+
+// text typing
+var typed = new Typed(".text-type", {
+  strings: ["Web Developer", "Front-end Developer"],
+  typeSpeed: 500,
+  backSpeed: 500,
+  loop: true,
+});
